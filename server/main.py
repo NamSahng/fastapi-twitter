39425dac1e4d8627ee1dc22de07afa187a40b2da
NamSahng <namskygreen@naver.com>
2022-07-13 21:39:19 +0900

    Change port

--- server/main.py
+++ server/main.py
@@ -27,5 +27,5 @@ def create_app():
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=9999, reload=True)
     
